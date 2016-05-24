0cf29499a0c97c08917f92b2ff7958e9f08973a2
Baby Pepper <royxagain@gmail.com>
2016-05-23 23:42:16 -0700

    create courses_students join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524061329) do
+ActiveRecord::Schema.define(version: 20160524063930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20160524061329) do
     t.string   "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "student_id"
   end
 
   create_table "roles", force: :cascade do |t|
