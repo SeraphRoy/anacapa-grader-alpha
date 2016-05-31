d3e925c8c7dc9860597eeb9bd18c9c2fbb0c5a56
Baby Pepper <royxagain@gmail.com>
2016-05-30 21:42:47 -0700

    add a passing course controller test

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CoursesController, type: :controller do
+  setup do
+    @student = ActionController::Parameters.new(email: "foo@gmail.com", studentid: "1234567", first_name: "Joe", last_name: "Gaucho")
+    student = Student.new(email: "foo@gmail.com", studentid: "1234567", first_name: "Joe", last_name: "Gaucho")
+    student.save
+    course = Course.new(dept: "Comp Science", num: "111", desc: "Blah", term:"S16")
+    course.save
+    @course = controller.new
+    @request.env['HTTP_REFERER'] = 'http://test.com/sessions/new'
+    post :add_student, :student => @student, :id => course.id
+  end
 
+  it "shoud add student to a course" do
+    response.should be_success
+  end
 end
